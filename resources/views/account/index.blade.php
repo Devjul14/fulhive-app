8e9194cb39ccc21b572e506be689e89d1c96806f
julia eka <eka47577@gmail.com>
2023-12-18 13:52:08 +0700

    ui team

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -2,24 +2,23 @@
 
 @section('content')
 <div class="container-fluid">
-    <h6 class="h3 mb-3 text-primary ml-3">Akun</h6>
+    <h6 class="h3 mb-3 text-gray-800 ml-3">Akun</h6>
         <div class="col-lg-12">
 
             <div class="card mb-4">
             <!-- Nav link -->
             <ul class="nav mt-3 ml-4">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Akun Saya</a>
+                    <a class="nav-link" href="#" id="akunLink">Akun Saya</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Tim Saya</a>
+                    <a class="nav-link" href="#" id="timLink">Tim Saya</a>
                 </li>
             </ul>
             <!-- card akun saya -->
-                <div class="card-body">
-                    <div class="row">
+            <div class="card-body">
+                    <div class="row" id="akunCard" style="display: none;">
                         <div class="col-lg-4">
-
                             <div class="card mb-4">
                                 <div class="card-body">
                                     <img src="https://via.placeholder.com/150" class="card-img-top rounded-circle mx-auto d-block" alt="Profile Photo">
@@ -31,7 +30,6 @@
                         </div>
 
                         <div class="col-lg-8">
-
                                 <div class="card mb-4">
                                     <div class="card-body">
                                         <div class="card-header py-3 mb-4">
@@ -82,11 +80,82 @@
                             </div>
                         </div>
                     </div>
-                </div>
+                    <!-- end card akun saya -->
+
+                    <!-- card tim saya -->
+                    <div class="row ml-4" id="timCard" style="display: none;">
+                        <div class="col-lg-11">
+                            <div class="mb-4">
+                            <button type="button" class="btn btn-outline-primary"><i class="fas fa-solid fa-user-plus"></i>  Tambah Tim</button>
+                            </div>
+                            <table class="table">
+                                <thead class="thead-light">
+                                    <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Nama Personil</th>
+                                    <th scope="col">No Telepon</th>
+                                    <th scope="col">Jenis Kelamin</th>
+                                    <th scope="col">Jenis Akses</th>
+                                    <th scope="col">Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                    <th scope="row">1</th>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                    </tr>
+                                    <tr>
+                                    <th scope="row">2</th>
+                                    <td>Jacob</td>
+                                    <td>Thornton</td>
+                                    <td>@fat</td>
+                                    </tr>
+                                    <tr>
+                                    <th scope="row">3</th>
+                                    <td>Larry</td>
+                                    <td>the Bird</td>
+                                    <td>@twitter</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <!-- end card tim saya -->
                 </div>
             </div>
-            <!-- end card akun saya -->
         </div>
     </div>
 </div>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+<script>
+  // Inisialisasi saat halaman dimuat
+  $(document).ready(function() {
+    // Atur fungsi klik untuk tautan "Akun Saya"
+    $("#akunLink").addClass("active");
+    $("#akunCard").show();
+
+    $("#akunLink").click(function() {
+      // Aktifkan tautan dan tampilkan kartu "Akun Saya"
+      $("#akunLink").addClass("active");
+      $("#timLink").removeClass("active");
+      $("#akunCard").show();
+      $("#timCard").hide();
+    });
+
+    // Atur fungsi klik untuk tautan "Tim Saya"
+    $("#timLink").click(function() {
+      // Aktifkan tautan dan tampilkan kartu "Tim Saya"
+      $("#timLink").addClass("active");
+      $("#akunLink").removeClass("active");
+      $("#timCard").show();
+      $("#akunCard").hide();
+    });
+  });
+</script>
 @endsection
